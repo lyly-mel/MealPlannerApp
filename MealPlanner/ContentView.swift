17c3e5079c51e2e446dee8c29540dc39c5fc4877
lylia melahi <lylia.melahi.91@hotmail.com>
2024-11-25 00:15:04 -0800

    Initial Commit

---
+++ MealPlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MealPlanner
+//
+//  Created by lylia melahi on 11/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MealPlanner/MealPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlannerApp.swift
+//  MealPlanner
+//
+//  Created by lylia melahi on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MealPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
